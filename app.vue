3a45504eb60c2d40608f494282e010185a32ef35
Hartaithan <hartaithan@gmail.com>
2023-12-30 19:32:19 +0900

    change app theme

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    Hello World!
-  </div>
+  <UInput v-model="value" placeholder="Search..." />
+  <UButton class="mt-3 mx-auto w-32 justify-center">Hello World!</UButton>
 </template>
+
+<script setup lang="ts">
+const value = ref('');
+</script>
