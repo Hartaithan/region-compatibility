98dc09f2fab277fc530e3804ef3c984fd87346c5
Hartaithan <hartaithan@gmail.com>
2024-01-04 12:05:58 +0900

    remove pseudoelements from displaying id parts

--- components/ComparisonView.vue
+++ components/ComparisonView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { CompareState, CompareStatus, ExtendedCompareState } from '~/models/compare'
+import type { CompareState, ExtendedCompareState } from '~/models/compare'
 
 interface Props {
   compare: CompareState
@@ -21,16 +21,32 @@ watch(compare, () => {
 </script>
 
 <template>
-  <UCard class="w-full h-32 flex justify-center items-center overflow-auto" :ui="{ body: { padding: 'flex h-full grow flex-col items-center justify-center sm:p-4 px-4 py-4' } }">
+  <UCard class="w-full h-24 flex justify-center items-center overflow-auto" :ui="{ body: { padding: 'flex h-full grow flex-col items-center justify-center sm:p-4 px-4 py-4' } }">
     <p v-if="extended.value.left === null && extended.value.right === null" class="text-xl">
       There's nothing to compare.
     </p>
     <div v-else class="w-full flex flex-col gap-2 justify-center items-center">
-      <UBadge v-if="extended.value.left" :before="`${extended.value.left.parts[0]}-`" :after="`-${extended.value.left.parts[2]}`" :class="`!text-white before:text-white after:text-white py-0.5 px-1 ${isCompatible.value ? '!bg-green-700' : '!bg-red-700'} text-md relative whitespace-pre before:absolute before:right-[97%] before:content-[attr(before)] after:absolute after:left-[97%] after:content-[attr(after)]`">
-        {{ extended.value.left.parts[1] }}
+      <UBadge v-if="extended.value.left" :class="`py-0.5 px-1 text-md relative whitespace-pre ${isCompatible.value ? '!bg-green-700' : '!bg-red-700'} `">
+        <p class="absolute right-[97%]">
+          {{ `${extended.value.left.parts[0]}-` }}
+        </p>
+        <p>
+          {{ extended.value.left.parts[1] }}
+        </p>
+        <p class="absolute left-[97%]">
+          {{ `-${extended.value.left.parts[2]}` }}
+        </p>
       </UBadge>
-      <UBadge v-if="extended.value.right" :before="`${extended.value.right.parts[0]}-`" :after="`-${extended.value.right.parts[2]}`" :class="`!text-white before:text-white after:text-white py-0.5 px-1 ${isCompatible.value ? '!bg-green-700' : '!bg-red-700'} text-md relative whitespace-pre before:absolute before:right-[97%] before:content-[attr(before)] after:absolute after:left-[97%] after:content-[attr(after)]`">
-        {{ extended.value.right.parts[1] }}
+      <UBadge v-if="extended.value.right" :class="`py-0.5 px-1 text-md relative whitespace-pre ${isCompatible.value ? '!bg-green-700' : '!bg-red-700'}`">
+        <p class="absolute right-[97%]">
+          {{ `${extended.value.right.parts[0]}-` }}
+        </p>
+        <p>
+          {{ extended.value.right.parts[1] }}
+        </p>
+        <p class="absolute left-[97%]">
+          {{ `-${extended.value.right.parts[2]}` }}
+        </p>
       </UBadge>
     </div>
   </UCard>
