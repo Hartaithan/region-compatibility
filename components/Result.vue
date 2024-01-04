dbd73943ffbc3d0898f66cfc6091ffcf0a060084
Hartaithan <hartaithan@gmail.com>
2024-01-04 13:01:58 +0900

    fix checkbox controlled state

--- components/Result.vue
+++ components/Result.vue
@@ -9,7 +9,6 @@ interface Props {
 
 const { link, target } = defineProps<Props>()
 const { compare, setCompare } = inject<CompareProvider>('compare') as CompareProvider
-const value = compare.value[target]
 
 function onChange(event: Event) {
   const { checked } = event.target as HTMLInputElement
@@ -19,7 +18,7 @@ function onChange(event: Event) {
 
 <template>
   <div class="flex items-center gap-3 p-3 bg-neutral border border-neutral-800 rounded-lg">
-    <UCheckbox name="active" :checked="link.id === value?.id" @change="onChange($event)" />
+    <UCheckbox name="active" :checked="link.id === compare[target]?.id" @change="onChange($event)" />
     <img class="min-h-16 h-16 min-w-16 aspect-square rounded-lg" :src="link.images[0].url" :alt="link.name">
     <div>
       <p class="text-sm font-bold">
