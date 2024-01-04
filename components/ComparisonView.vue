a288ab69a5848ec0eaccfb5585cd0a182a0b9624
Hartaithan <hartaithan@gmail.com>
2024-01-04 13:01:35 +0900

    fix colors classes load

--- components/ComparisonView.vue
+++ components/ComparisonView.vue
@@ -14,18 +14,23 @@ const { compare } = defineProps<Props>()
 const extended = reactive<Reactive<ExtendedCompareState>>({ value: compare as ExtendedCompareState })
 
 watch(compare, () => {
-  const value = extendCompare(compare)
-  extended.value = value
+  extended.value = extendCompare(compare)
 })
 </script>
 
 <template>
-  <UCard class="w-full h-24 flex justify-center items-center overflow-auto" :ui="{ body: { padding: 'flex h-full grow flex-col items-center justify-center sm:p-4 px-4 py-4' } }">
+  <UCard
+    class="w-full h-24 flex justify-center items-center overflow-auto"
+    :ui="{ body: { padding: 'flex h-full grow flex-col items-center justify-center sm:p-4 px-4 py-4' } }"
+  >
     <p v-if="extended.value.left === null && extended.value.right === null" class="text-xl">
       There's nothing to compare.
     </p>
     <div v-else class="w-full flex flex-col gap-2 justify-center items-center">
-      <UBadge v-if="extended.value.left" :class="`py-0.5 px-1 text-md relative whitespace-pre ${statusColors[extended.value.status]}`">
+      <UBadge
+        v-if="extended.value.left"
+        :class="`py-0.5 px-1 text-md relative whitespace-pre ${statusColors[extended.value.status]}`"
+      >
         <p class="absolute right-[97%]">
           {{ `${extended.value.left.parts[0]}-` }}
         </p>
@@ -36,7 +41,10 @@ watch(compare, () => {
           {{ `-${extended.value.left.parts[2]}` }}
         </p>
       </UBadge>
-      <UBadge v-if="extended.value.right" :class="`py-0.5 px-1 text-md relative whitespace-pre ${statusColors[extended.value.status]}`">
+      <UBadge
+        v-if="extended.value.right"
+        :class="`py-0.5 px-1 text-md relative whitespace-pre ${statusColors[extended.value.status]}`"
+      >
         <p class="absolute right-[97%]">
           {{ `${extended.value.right.parts[0]}-` }}
         </p>
@@ -48,5 +56,8 @@ watch(compare, () => {
         </p>
       </UBadge>
     </div>
+    <div class="hidden bg-slate-700" />
+    <div class="hidden bg-green-700" />
+    <div class="hidden bg-red-700" />
   </UCard>
 </template>
