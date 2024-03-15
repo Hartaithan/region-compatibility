8bd48fb91b94294b817fe5d404b8ab781fc98e97
Hartaithan <hartaithan@gmail.com>
2024-03-15 10:51:59 +0900

    install shadcn

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import { Button } from './components/ui/button'
+</script>
+
 <template>
-  Hello World!
+  <main class="h-full flex flex-col justify-center items-center">
+    <Button>Hello World!</Button>
+  </main>
 </template>
+
+<style scoped>
+
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { URL, fileURLToPath } from 'node:url'
-
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
