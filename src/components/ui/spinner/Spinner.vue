6a1bba5abb676195258f4021e4785261f6b0f45d
Hartaithan <hartaithan@gmail.com>
2024-03-28 10:55:48 +0900

    extend spinner component props

--- src/components/ui/spinner/Spinner.vue
+++ src/components/ui/spinner/Spinner.vue
@@ -2,9 +2,10 @@
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/utils/classes'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   class?: HTMLAttributes['class']
-}>()
+  strokeWidth?: number
+}>(), { strokeWidth: 3 })
 </script>
 
 <template>
@@ -16,9 +17,9 @@ const props = defineProps<{
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    :stroke-width="strokeWidth"
+    stroke-linecap="round"
+    stroke-linejoin="round"
   >
     <path d="M21 12a9 9 0 1 1-6.219-8.56" />
   </svg>
